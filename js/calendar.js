cb32b5af5c0b6e0a52ed2aad49e7d260fd8d24ea
Noah-TaeHwan <noah.taehwan@gmail.com>
2024-01-05 07:35:04 +0900

    clock and calendar

---
+++ js/calendar.js
@@ -0,0 +1,47 @@
+const calendarDays = document.querySelector('.column__info--days');
+const calendarDate = document.querySelector('.column__info--date');
+
+console.log(calendarDays);
+console.log(calendarDate);
+
+function getDays() {
+  const date = new Date();
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const day = daysOfWeek[date.getDay()];
+
+  calendarDays.innerText = `${day}`;
+}
+
+function getDate() {
+  const date = new Date();
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const month = months[date.getMonth()];
+  const day = date.getDay();
+  const year = String(date.getFullYear());
+
+  calendarDate.innerText = `${month} ${day}, ${year}`;
+}
+
+getDays();
+getDate();
