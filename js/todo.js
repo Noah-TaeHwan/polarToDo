50d2e78ccfb507273f06b74d994331474cb32767
Noah-TaeHwan <noah.taehwan@gmail.com>
2024-01-06 17:10:45 +0900

    Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,21 @@
 const toDoForm = document.querySelector('.column__toDos');
 const toDoInput = document.querySelector('.column__toDos input');
 
-const toDoList = document.querySelector('.column__toDo-list');
+const toDoList = document.querySelector('.column__todo-list');
+
+function paintToDo(newTodo) {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = '';
-  console.log(newTodo);
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
