8941210211efa519129b598c8ed23abdef310422
Noah-TaeHwan <noah.taehwan@gmail.com>
2024-01-06 16:52:19 +0900

    todo refresh

--- js/greetings.js
+++ js/greetings.js
@@ -4,6 +4,7 @@ const loginBtn = document.querySelector('.login-btn');
 const loginInput = document.querySelector('.column__logIn input');
 const greeting = document.querySelector('#greeting');
 const todos = document.querySelector('.todos');
+const fixedToDos = document.querySelector('.column__toDos');
 
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = 'username';
@@ -15,6 +16,7 @@ function loginClick(event) {
   loginForm2.classList.add(HIDDEN_CLASSNAME);
   loginBtn.classList.add(HIDDEN_CLASSNAME);
   paintGreetings(username);
+  location.reload();
 }
 
 function paintGreetings(username) {
@@ -28,8 +30,10 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm2.classList.remove(HIDDEN_CLASSNAME);
+  todos.classList.add(HIDDEN_CLASSNAME);
 } else {
   paintGreetings(savedUsername);
   loginForm2.classList.add(HIDDEN_CLASSNAME);
   loginBtn.classList.add(HIDDEN_CLASSNAME);
+  todos.classList.remove(HIDDEN_CLASSNAME);
 }
