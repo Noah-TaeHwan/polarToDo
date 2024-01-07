f426962f38a2f36bb2e00429fa021043ab517204
Noah-TaeHwan <noah.taehwan@gmail.com>
2024-01-07 11:44:32 +0900

    fixing to add todos

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,20 @@ const toDoInput = document.querySelector('.column__toDos input');
 
 const toDoList = document.querySelector('.column__todo-list');
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement('li');
   const span = document.createElement('span');
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement('button');
+  button.innerText = '❌';
+  button.addEventListener('click', deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
