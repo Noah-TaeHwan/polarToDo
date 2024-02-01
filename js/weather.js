871f649d8c82e52f2b23feab84074033bf8bad67
Noah-TaeHwan <noah.taehwan@gmail.com>
2024-02-01 09:19:29 +0900

    8.1 Weather API

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = `d388e45698c1b44ad119dd5d2f1232ac`;
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert(`Can't Find You. No Weather For You!`);
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
